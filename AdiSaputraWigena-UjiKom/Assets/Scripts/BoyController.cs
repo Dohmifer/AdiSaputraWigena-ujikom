aa00d48481946295a49c4a4255e9b97041736736
Dohmifer <142681991+Dohmifer@users.noreply.github.com>
2024-12-18 10:09:06 +0700

    move Horizontal

--- AdiSaputraWigena-UjiKom/Assets/Scripts/BoyController.cs
+++ AdiSaputraWigena-UjiKom/Assets/Scripts/BoyController.cs
@@ -27,15 +27,25 @@ public class BoyController : MonoBehaviour
         // float moveHorizontal = Input.GetAxis("Horizontal");
         // float move = rb.velocity.x;
         // move = transform.Translate(transform.position.x * speed, transform.position.y, transform.position.z);
+        // float horizontal = Input.GetAxis("Horizontal");
+        // //float vertical = Input.GetAxis("Vertical");
+        // //float speed = 5.0f;
+        // transform.position = new Vector3(horizontal, 0) * speed * Time.deltaTime;
 
-        // if(moveHorizontal >= 0)
+        // if (horizontal >= 0)
         // {
-
+        //     transform.position = new Vector3(horizontal, 0) * speed * Time.deltaTime;
         // }
-        float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
-        float speed = 5.0f;
-        transform.position = new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;
+
+        float mHorizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(Input.GetAxis("Horizontal"));
+
+        Vector2 move = new Vector2(mHorizontal, rb.velocity.y);
+
+        rb.velocity = move * speed;
+
     }
+
+
 
 }
